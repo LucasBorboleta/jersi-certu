c4bd93c6bbd6ccb5ca8045da90b95748305dcb05
Lucas Borboleta <lucas.borboleta@free.fr>
2021-12-14 19:07:14 +0100

    Prepared version 1.3.2 for distribution as a zip archive

--- jersi_certu/jersi_rules.py
+++ jersi_certu/jersi_rules.py
@@ -3,7 +3,7 @@
 
 """jersi_rules.py implements the rules engine for the JERSI boardgame."""
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 
 _COPYRIGHT_AND_LICENSE = """
 JERSI-CERTU implements a GUI and a rules engine for the JERSI boardgame.
